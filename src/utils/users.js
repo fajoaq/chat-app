dbd0fc720dfa9ba19472c4dfc0e76821db2fc837
Francis J <fajoaq@gmail.com>
2021-06-17 13:59:28 -0400

    created user utility functions

---
+++ src/utils/users.js
@@ -0,0 +1,43 @@
+const users = [];
+
+//ADD USER
+const addUser = ({ id, username, room }) => {
+    //clean, validate
+    username = username.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    if(!username || !room) return { error: "Username and room are required." }
+
+    const existingUser = users.find((user) =>  user.room === room && user.username === username);
+
+    if(existingUser) return { error: "Username is in use!" }
+
+    // store user
+    const user = { id, username, room };
+
+    users.push(user);
+    return { user }
+}
+//REMOVE USER
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id);
+
+    if(index !== -1) {
+        return users.splice(index, 1)[0];
+    }
+};
+//GET USER
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+};
+//GET USERS IN ROOM
+const getUsersInRoom = (room) => {
+    return users.filter((user) => user.room === room.toLowerCase());
+};
+
+module.exports = {
+    addUser,
+    getUser,
+    removeUser,
+    getUsersInRoom
+}
