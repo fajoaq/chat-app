90294f192fb5716c035ccfddc6bb1680427f84fb
Francis J <fajoaq@gmail.com>
2021-06-21 12:12:47 -0400

    moved select tag out of template

--- public/js/join.js
+++ public/js/join.js
@@ -3,7 +3,7 @@ const socket = io();
 //ELEMENTS
 const $joinForm = document.querySelector('#join-form');
 const $roomInput = $joinForm.querySelector('input[name="room"]');
-let $selection;
+const $selection = document.querySelector('#open-rooms-dropdown');;
 //TEMPLATES
 const roomSelectionTemplate = document.querySelector('#room-selection-template').innerHTML;
 //ON CONNECTION
@@ -11,9 +11,8 @@ socket.on('joinForm', (roomList) => {
     //append selection template to form with data
     const html = Mustache.render(roomSelectionTemplate, { roomList });
 
-    $joinForm.querySelector('#room-selection').innerHTML = html;
+    $selection.innerHTML = html;
     
     //ON SELECTION CHANGE
-    $selection = document.querySelector('#open-rooms-dropdown');
     $selection.onchange = ({ target }) => $roomInput.value = target.value;
 });
